a914b1586c549bb0435b0e7076b49b05ef28bce8
Maiyer Thao <MJAT@Maiyers-iMac.local>
2018-01-08 21:24:03 -0600

    added logic

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,77 +1,42 @@
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-    function displayAnimalGif() {
+$(function(){
+  populateButtons(searchArray,'searchButton','#button-holder');
+});
 
-      var animal = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+var searchArray = ['elephant', 'parrot', 'bear', 'tiger'];
 
-      // Creates AJAX call for the specific animal button being clicked
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response) {
+function populateButtons(searchArray, classToAdd, areaToAdd){
+  $(areaToAdd).empty();
+  for(var i = 0;i<searchArray.length;i++){
+    var a = $('<button type="button" class="btn btn-info">');
+    a.addClass(classToAdd);
+    a.attr('data-type', searchArray[i]);
+    a.text(searchArray[i]);
+    $(areaToAdd).append(a);
+  }
+}
 
-        console.log(response);
-
-        // make a variable named results and set it equal to response.data
-        var results = response.data;
-
-        // for (var i = 0; i < results.length; i++) {
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $('<div class= "item">');
-
-          var rating = results[i].rating;
-
-          var p = $('<p>').text('Rating: ' + rating);
-          var animalImage = $('<img>');
-          animalImage.attr('src', results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-          gifDiv.prepend(animalImage);
-
-          $('#gifs').prepend(gifDiv);
-        }
-
-      });
-
-    // Function for displaying movie data
-    function renderButtons() {
-
-      // Deletes the movies prior to adding new movies
-      // (this is necessary otherwise you will have repeat buttons)
-      $("#buttons-view").empty();
-      // Loops through the array of movies
-      for (var i = 0; i < animal.length; i++) {
-
-        // Then dynamicaly generates buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adds a class of movie to our button
-        a.addClass("animal");
-        // Added a data-attribute
-        a.attr("data-name", animal[i]);
-        // Provided the initial button text
-        a.text(animal[i]);
-        // Added the button to the buttons-view div
-        $("#gifs").append(a);
-      }
+$(document).on('click','.searchButton',function(){
+  var type = $(this).data('type');
+  var queryURL = 'http:///api.giphy.com/v1/gifs/search?q='+type+
+  '&api_key=X5KyOyApsC3FaBlvx3X5F8wpQxp06YGz&limit=10';
+  $.ajax({url:queryURL,method:"GET"})
+  .done(function(response){
+    console.log(response);
+    for(var i = 0;i<response.data.length;i++){
+      var searchDiv = $('<div class="search-item">');
+      var rating = response.data[i].rating;
+      var p = $('<p>').text('Rating: '+rating);
+      var animated = response.data[i].images.fixed_height.url;
+      var still = response.data[i].images.fixed_height_still.url;
+      var image = $('<img>');
+      image.attr('src',still);
+      image.attr('data-still',still);
+      image.attr('data-animated',animated);
+      image.attr('data-state','still');
+      image.addClass('searchImage');
+      searchDiv.append(p);
+      searchDiv.append(image);
+      $('#gifs').append(searchDiv);
     }
-
-    // This function handles events where the add movie button is clicked
-    $("#submit").on("click", function(event) {
-      event.preventDefault();
-      // This line of code will grab the input from the textbox
-      var animal = $("#search").val().trim();
-
-      // The movie from the textbox is then added to our array
-      movies.push(animal);
-
-      // Calling renderButtons which handles the processing of our movie array
-      renderButtons();
-    });
-
-    // Adding click event listeners to all elements with a class of "movie"
-    $(document).on("click", ".movie", displayMovieInfo);
-
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+  })
+})
